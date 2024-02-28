9831893f51a697237e090dd0aa982775724efa99
Dmitriy <dikorpshy@mail.ru>
2024-02-28 16:50:47 +0300

    add test and ch c1

---
+++ Rectangle_1/Line_C/C1.py
@@ -0,0 +1,22 @@
+
+
+def color_rect():
+
+    t: int
+    n: int
+    s: list
+
+    t = int(input("t = "))
+    n = int(input("n = "))
+    s = input("str = ").split('')
+
+
+    for i in range(len(s)):
+        #s[i] = 
+        print(s[i])
+    
+    return 0
+
+
+
+color_rect()
---
+++ Rectangle_1/Line_C/test_c.py
@@ -0,0 +1,8 @@
+import C1
+
+def test_str():
+    t: int = 1
+    n: int = 2
+    s: str = "BR"
+
+    assert C1.color_rect() == 0
